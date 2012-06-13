1c5f412f4a2929f4a56bb10554f9418e63aef6f6
Kelly Sumrall <ksumrall@totalsafety.com>
2012-06-13 16:32:51 -0500

    adding more files

---
+++ DataTierGenerator.Common/IView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TotalSafety.DataTierGenerator.Common
+{
+    public interface IView
+    {
+        string Name { get; set; }
+        string DatabaseName { get; set; }
+        string Description { get; set; }
+        string Schema { get; set; }
+        bool Build { get; set; }
+        Column[] Columns { get; set; }
+    }
+}
